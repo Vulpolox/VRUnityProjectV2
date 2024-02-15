57dfe39c071fc69533442956205c097fd897a975
Vulpolox <jack.w.a.keys@gmail.com>
2024-02-15 01:22:49 -0600

    Fixed death sound not playing

--- PuzzlePlatformerPersonalProjectJK/Assets/Scripts/DeathHandler.cs
+++ PuzzlePlatformerPersonalProjectJK/Assets/Scripts/DeathHandler.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
 
-[RequireComponent (typeof(AudioSource))]
-
 public class DeathHandler : MonoBehaviour
 {
     [Tooltip("The height at which the player dies and the level resets")]
@@ -15,14 +13,21 @@ public class DeathHandler : MonoBehaviour
     [Tooltip("The sound to play when the player dies")]
     public AudioClip deathSound = null;
 
-    private AudioSource audioSource = null;          // The AudioSource the emit sounds
-    public XROrigin xrRig = null;                    // The player
+    [Tooltip("The XR Rig; this is the only object whose movement is tracked")]
+    public XROrigin xrRig = null;
+
+    private AudioSource audioSource = null;   // The thing that emits sounds
+    private float soundHeight;                // The height at which the death sound plays
+    private bool soundFlag = false;
+    
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        audioSource = xrRig.GetComponent<AudioSource>();
+        soundHeight = killHeight + 20;
     }
 
     // Update is called once per frame
@@ -32,19 +37,22 @@ public class DeathHandler : MonoBehaviour
         {
             KillPlayer();
         }
+
+        else if (xrRig.transform.position.y <= soundHeight && soundFlag == false)
+        {
+            PlayDeathSound();
+        }
     }
 
     public void KillPlayer()
     {
-        audioSource.PlayOneShot(deathSound);                        // play the death sound
-        StartCoroutine(waitSecond(1f));                             // wait 1 second for the death sound to play
         // TODO: add UI menu where user chooses to restart or to quit to level select //
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // reload the scene
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // reload the scene (temporary)
     }
 
-    IEnumerator waitSecond(float sec)
+    public void PlayDeathSound()
     {
-        yield return new WaitForSeconds(sec);
+        audioSource.PlayOneShot(deathSound);
+        soundFlag = true;
     }
 }
