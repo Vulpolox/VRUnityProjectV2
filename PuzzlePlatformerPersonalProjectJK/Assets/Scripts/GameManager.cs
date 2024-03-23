e0031a6889a4d4a9460cdb810b3e5e7ac6cc5387
Vulpolox <jack.w.a.keys@gmail.com>
2024-03-23 03:46:01 -0500

    Got color state working

--- PuzzlePlatformerPersonalProjectJK/Assets/Scripts/GameManager.cs
+++ PuzzlePlatformerPersonalProjectJK/Assets/Scripts/GameManager.cs
@@ -7,19 +7,19 @@ public class GameManager : MonoBehaviour
 {
     // GameObject References
         [Tooltip("Where the player starts the level")]
-        public GameObject startPoint;            // to hold reference to the startPoint
+        public GameObject startPoint;            // to hold reference to the startPoint; where the player spawns in the level
 
         [Tooltip("The player")]
         public XROrigin player;                  // to hold reference to the player
 
-        private GameObject leftHand, rightHand;  // to hold references to the hands of the XROrigin
+        private Renderer leftRenderer, rightRenderer;
 
     // StartPoint Variables
         private Vector3 startCoords;             // to hold the x and z coordinates of the start point
         private float playerHeight = 1.3f;       // y offset of the player to prevent spawning in the ground
 
     // Color Switcher Variables
-        private Color currentPlayerColor = Color.white;
+        private Color currentPlayerColor = Color.black;
     
         [Tooltip("The color the player starts as")]
         public Color startingColor;
@@ -27,16 +27,30 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         // get references
-        player = GameObject.FindWithTag("Player").GetComponent<XROrigin>(); // automatically set the reference to the player if it exists
-        startPoint = GameObject.FindWithTag("StartPoint");                  // automatically set the reference to the start point if it exists
-        leftHand = GameObject.FindWithTag("LeftHand");                      // do the same for the left hand
-        rightHand = GameObject.FindWithTag("RightHand");                    // do the same for the right hand
+        player = GameObject.FindWithTag("Player").GetComponent<XROrigin>();            // automatically set the reference to the player if it exists
+        startPoint = GameObject.FindWithTag("StartPoint");                             // automatically set the reference to the startPoint if it exists
+        leftRenderer = GameObject.FindWithTag("LeftHand").GetComponent<Renderer>();    // do the same for the left hand renderer
+        rightRenderer = GameObject.FindWithTag("RightHand").GetComponent<Renderer>();  // do the same for the right hand renderer
 
 
         // exit the function if references aren't set properly
         if (startPoint == null || player == null)
-            return;                                                         
+        {
+            Debug.Log("References not set properly");
+            return;
+        }
+            
+        // spawn the player at the startPoint
+        SpawnPlayer();
 
+        // change player's currentColor to the startingColor
+        ChangeColor(startingColor);
+        
+    }
+
+    // method for setting the player's position and rotation to that of the startPoint
+    private void SpawnPlayer()
+    {
         // get the location for the player to spawn in
         startCoords = startPoint.transform.position;
         startCoords.y += playerHeight;
@@ -46,9 +60,12 @@ public class GameManager : MonoBehaviour
         player.transform.rotation = startPoint.transform.rotation;
     }
 
+    // method for changing the player's color state
     public void ChangeColor(Color newColor)
     {
-
+        // change the color of the player's hands to signify the current color state
+        leftRenderer.material.color = newColor;
+        rightRenderer.material.color = newColor;
     }
 
 }
